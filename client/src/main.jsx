500002c773b4bd9ce8856b43c0222875a47160d5
vanshika sahu <vanshikasahu281105@gmail.com>
2025-10-24 11:19:18 +0530

    new changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
 import {  AppContextProvider } from './Context/AppContext.jsx'
 import {ClerkProvider} from '@clerk/clerk-react'
+
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
