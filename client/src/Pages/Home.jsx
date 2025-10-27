e8208d750b7f8fc48a98f7eb594fb9b67c3da7e6
vanshika sahu <vanshikasahu281105@gmail.com>
2025-10-27 07:11:02 +0530

    current Search Section

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "../Components/NavBar";
 import Hero from "../Components/Hero";
+import JobListing from "../Components/JobListing";
 
 const Home = () => {
   return (
     <div>
       <NavBar />
       <Hero />
+      <JobListing />
     </div>
   );
 };
