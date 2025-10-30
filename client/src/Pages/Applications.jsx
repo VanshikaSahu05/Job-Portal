13ebe4bb980290ba9d56a8ec2ea0bc54571c375e
vanshika sahu <vanshikasahu281105@gmail.com>
2025-10-30 08:52:07 +0530

    Application page is completed

--- client/src/Pages/Applications.jsx
+++ client/src/Pages/Applications.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import NavBar from "../Components/NavBar";
-import { assets } from "../assets/assets";
+import { assets, jobsApplied } from "../assets/assets";
+import moment from "moment";
+import Footer from "../Components/Footer";
 
 const Applications = () => {
   const [isEdit, setIsEdit] = useState(false);
@@ -12,23 +14,29 @@ const Applications = () => {
       <div className="container px-4 min-h-[65vh] 2xl:px-20 mx-auto my-10">
         <h2 className="text-xl font-semibold">Your Resume</h2>
         <div className="flex gap-2 mb-6 mt-3">
-          {
-          isEdit 
-          ? <>
+          {isEdit ? (
+            <>
               <label className="flex items-center" htmlFor="ResumeUpload">
-                <p className="bg-blue-100 text-blue-600 px-4 py-2 rounded-lg mr-2">Select Resume</p>
+                <p className="bg-blue-100 text-blue-600 px-4 py-2 rounded-lg mr-2">
+                  Select Resume
+                </p>
                 <input
                   id="ResumeUpload"
-                  onChange={e => setResume(e.target.files[0])}
+                  onChange={(e) => setResume(e.target.files[0])}
                   accept="application/pdf"
                   type="file"
                   hidden
                 />
                 <img src={assets.profile_upload_icon} alt="" />
               </label>
-              <button onClick={e => setIsEdit(false)} className="bg-green-100 border border-green-400 rounded-lg px-4 py-2">Save</button>
+              <button
+                onClick={(e) => setIsEdit(false)}
+                className="bg-green-100 border border-green-400 rounded-lg px-4 py-2"
+              >
+                Save
+              </button>
             </>
-           : (
+          ) : (
             <div className="flex gap-2">
               <a
                 className="bg-blue-100 text-blue-600 px-4 py-2 rounded-lg"
@@ -45,7 +53,65 @@ const Applications = () => {
             </div>
           )}
         </div>
+
+        <h2 className="text-xl font-semibold mb-4">Jobs Applied</h2>
+        <table className="min-w-full bg-white border border-gray-300 rounded-lg">
+          <thead>
+            <tr>
+              <th className="py-3 px-4 border-b border-gray-300 text-left">
+                Company
+              </th>
+              <th className="py-3 px-4 border-b border-gray-300 text-left">
+                Job Title
+              </th>
+              <th className="py-3 px-4 border-b border-gray-300 text-left max-sm:hidden">
+                Location
+              </th>
+              <th className="py-3 px-4 border-b border-gray-300 text-left max-sm:hidden">
+                Date
+              </th>
+              <th className="py-3 px-4 border-b border-gray-300 text-left">
+                Status
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {jobsApplied.map((job, index) =>
+              true ? (
+                <tr>
+                  <td className="py-3 px-4 flex items-center gap-2 border-b border-gray-300">
+                    <img className="w-8 h-8" src={job.logo} alt="" />{" "}
+                    {job.company}
+                  </td>
+                  <td className="py-2 px-4 border-b border-gray-300">
+                    {job.title}
+                  </td>
+                  <td className="py-2 px-4 border-b border-gray-300 max-sm:hidden">
+                    {job.location}
+                  </td>
+                  <td className="py-2 px-4 border-b border-gray-300 max-sm:hidden">
+                    {moment(job.date).format("ll")}
+                  </td>
+                  <td className="py-2 px-4 border-b border-gray-300">
+                    <span
+                      className={`${
+                        job.status === "Accepted"
+                          ? "bg-green-100"
+                          : job.status === "Rejected"
+                          ? "bg-red-100"
+                          : "bg-blue-100"
+                      } px-4 py-1.5 rounded`}
+                    >
+                      {job.status}
+                    </span>
+                  </td>
+                </tr>
+              ) : null
+            )}
+          </tbody>
+        </table>
       </div>
+      <Footer />
     </>
   );
 };
