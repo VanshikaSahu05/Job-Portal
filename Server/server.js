2e9c561fa35aee47a2e23bbd70265c7733fd1be9
vanshika sahu <vanshikasahu281105@gmail.com>
2025-11-06 15:10:00 +0530

    latest webhook work for clerk

--- Server/server.js
+++ Server/server.js
@@ -1,46 +1,54 @@
-import './config/instrument.js'
-import 'dotenv/config' 
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose';
-import * as Sentry from "@sentry/node"
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import * as Sentry from "@sentry/node";
+import { ClerkWebHooks } from "./controllers/webhooks.js";
 
 // import connectDB from './config/db.js';
 
 // Initialize  express
 
 const app = express();
+app.use(express.json());
 
-const dBurl = process.env.MONGODB_URI;
+console.log("here server.js is going to start");
+
+const dBurl =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://vanshikasahu281105_db_user:g03E9Uey4YKWnkVd@cluster0.d7p8cot.mongodb.net/?appName=Cluster0";
+
+  console.log("session end");
 
 async function connectDB() {
-    try{
-        await mongoose.connect(dBurl);
-        console.log("connected");
-    }
-    catch(err){
-        console.log("err",err);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(dBurl);
+    console.log("Database connected successfully");
+  } catch (err) {
+    console.log("Database connection error:", err);
+    process.exit(1);
+  }
 }
 
 // connect to database
-await connectDB()
+await connectDB();
+
 
 // Middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Routes
-app.get('/',(req,res)=>res.send("API working"))
+app.get("/", (req, res) => res.send("API working"));
 app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
+app.post("/webhooks", ClerkWebHooks);
 
-// port 
+// port
 const PORT = process.env.PORT || 5000;
 Sentry.setupExpressErrorHandler(app);
 
-app.listen(PORT,() => {
-    console.log('Server is running on port:' + PORT);
-})
+app.listen(PORT, () => {
+  console.log("Server is running on port:" + PORT);
+});
